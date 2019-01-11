59d0179b7eda1b03e16226e4d1f24f81063dab25
zealotzealot <hsh0908y@gmail.com>
2019-01-11 17:57:36 +0900

    Create models TesterOrder

--- pida/testers/models.py
+++ pida/testers/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from products.models import Product, Category
+
+
+class TesterOrder(models.Model):
+    STATUS_CHOICES = (
+        (1, 'Preparing'),
+        (2, 'Shipping'),
+        (3, 'Delivered'),
+    )
+
+    owner = models.ForeignKey(User,
+                              on_delete=models.CASCADE,
+                              related_name='tester_orders')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.PROTECT,
+                                 related_name='+')
+    products = models.ManyToManyField(Product,
+                                      related_name='+')
+    order_time = models.DateTimeField(auto_now_add=True)
+    price = models.PositiveIntegerField()
+    status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES,
+                                              default=STATUS_CHOICES[0][0])
+
+    def is_valid(self):
+        if self.status not in [c[0] for c in self.STATUS_CHOICES]:
+            return False
+        if self.products.count() != 3:
+            return False
+        if any(p.category!=self.category for p in self.products.all()):
+            return False
+        return True
