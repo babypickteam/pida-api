7ad08a879bfc447a351a61bccb0d662d9f007dc6
zealotzealot <hsh0908y@gmail.com>
2019-01-16 16:59:48 +0900

    Add unique_together of GroupPurchaseOrder

--- pida/grouppurchases/models.py
+++ pida/grouppurchases/models.py
@@ -23,6 +23,9 @@ class GroupPurchaseOrder(models.Model):
     status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES,
                                               default=STATUS_CHOICES[0][0])
 
+    class Meta:
+        unique_together = (('owner', 'event'),)
+
     def is_valid(self):
         return self.status in [c[0] for c in self.STATUS_CHOICES] \
                and self.quantity > 0
