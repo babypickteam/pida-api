de5424d66302c98df221a8c7e181073395cde0cd
zealotzealot <hsh0908y@gmail.com>
2019-01-16 18:29:00 +0900

    Implement and setup views for app testers

--- pida/testers/urls.py
+++ pida/testers/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 
 urlpatterns = [
+    path('tester-orders/',
+         views.TesterOrderList.as_view(),
+         name='tester-order-list'),
+    path('tester-orders/<int:pk>/',
+         views.TesterOrderDetail.as_view(),
+         name='tester-order-detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
