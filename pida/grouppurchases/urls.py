0b53adc8b360d114946d90e6e9a650ee4889f039
zealotzealot <hsh0908y@gmail.com>
2019-01-17 11:25:17 +0900

    Implement and setup views for app grouppurchases

--- pida/grouppurchases/urls.py
+++ pida/grouppurchases/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 
 urlpatterns = [
+    path('group-purchase-orders/',
+         views.GroupPurchaseOrderList.as_view(),
+         name='group-purchase-order-list'),
+    path('group-purchase-orders/<int:pk>/',
+         views.GroupPurchaseOrderDetail.as_view(),
+         name='group-purchase-order-detail'),
+    path('group-purchase-events/',
+         views.GroupPurchaseEventList.as_view(),
+         name='group-purchase-event-list'),
+    path('group-purchase-events/<int:pk>/',
+         views.GroupPurchaseEventDetail.as_view(),
+         name='group-purchase-event-detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- pida/grouppurchases/views.py
+++ pida/grouppurchases/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import GroupPurchaseOrder, GroupPurchaseEvent
+from .serializers import GroupPurchaseOrderSerializer, GroupPurchaseEventSerializer
+
+
+class GroupPurchaseOrderList(generics.CreateAPIView):
+    queryset = GroupPurchaseOrder.objects.all()
+    serializer_class = GroupPurchaseOrderSerializer
+
+
+class GroupPurchaseOrderDetail(generics.RetrieveAPIView):
+    queryset = GroupPurchaseOrder.objects.all()
+    serializer_class = GroupPurchaseOrderSerializer
+
+
+class GroupPurchaseEventList(generics.ListAPIView):
+    queryset = GroupPurchaseEvent.objects.all()
+    serializer_class = GroupPurchaseEventSerializer
+
+
+class GroupPurchaseEventDetail(generics.RetrieveAPIView):
+    queryset = GroupPurchaseEvent.objects.all()
+    serializer_class = GroupPurchaseEventSerializer
