5933ebae85f312c42eb89c48ef51a47cb0bc53c6
zealotzealot <hsh0908y@gmail.com>
2019-01-17 11:08:47 +0900

    Implement and setup views for app purchases

--- pida/purchases/views.py
+++ pida/purchases/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import PurchaseOrder
+from .serializers import PurchaseOrderSerializer
+
+
+class PurchaseOrderList(generics.CreateAPIView):
+    queryset = PurchaseOrder.objects.all()
+    serializer_class = PurchaseOrderSerializer
+
+
+class PurchaseOrderDetail(generics.RetrieveAPIView):
+    queryset = PurchaseOrder.objects.all()
+    serializer_class = PurchaseOrderSerializer
