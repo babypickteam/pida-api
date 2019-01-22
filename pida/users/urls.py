02eb6bf5c3e41d1069a7c033f4a82e3c91c7ab97
zealotzealot <hsh0908y@gmail.com>
2019-01-22 16:38:27 +0900

    Implement views, urls, and related permissions for User

--- pida/users/urls.py
+++ pida/users/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 
 urlpatterns = [
+    path('users/<str:username>/',
+         views.UserDetail.as_view(),
+         name='user-detail'),
     path('payment-informations/<int:pk>/',
          views.PaymentInformationDetail.as_view(),
          name='payment-information-detail'),
--- pida/users/views.py
+++ pida/users/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from rest_framework import generics
 
 from common.permissions import IsAuthenticatedOwner
-from .models import PaymentInformation, DeliveryInformation
-from .serializers import PaymentInformationSerializer, DeliveryInformationSerializer
+from .models import User, PaymentInformation, DeliveryInformation
+from .permissions import IsSelf
+from .serializers import UserSerializer, PaymentInformationSerializer, DeliveryInformationSerializer
+
+
+class UserDetail(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsSelf,)
+    lookup_field = 'username'
 
 
 class PaymentInformationDetail(generics.RetrieveUpdateAPIView):
