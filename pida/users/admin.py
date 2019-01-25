a3c81183a09b041fbda97f047825f22d604f7c4c
zealotzealot <hsh0908y@gmail.com>
2019-01-25 18:26:48 +0900

    Add missing models in admin of app users

--- pida/users/admin.py
+++ pida/users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, SkinConcern, Allergy, PaymentInformation, DeliveryInformation
 
 
 admin.site.register(User)
+admin.site.register(SkinConcern)
+admin.site.register(Allergy)
+admin.site.register(PaymentInformation)
+admin.site.register(DeliveryInformation)
