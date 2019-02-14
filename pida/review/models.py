0fd8c5a859cae0cde00c03fdd1ff436526f5178d
zealotzealot <hsh0908y@gmail.com>
2019-02-14 18:59:11 +0900

    Create model Review and implement related serializer

--- pida/review/models.py
+++ pida/review/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+from products.models import Product
+
+
+class Review(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE,
+                              related_name='reviews')
+    product = models.ForeignKey(Product,
+                                on_delete=models.PROTECT,
+                                related_name='reviews')
+    content = models.TextField()
+    written_time = models.DateTimeField(auto_now_add=True)
