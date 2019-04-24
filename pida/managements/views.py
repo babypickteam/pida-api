bfbfe138fd2724efd2e42c2ba92ba99cb69c0cb3
zealotzealot <hsh0908y@gmail.com>
2019-04-24 18:41:24 +0900

    Add views and urls for Agreement

--- pida/managements/views.py
+++ pida/managements/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 
-from .models import Notice, Faq
-from .serializers import NoticeSerializer, FaqSerializer
+from .models import Notice, Faq, Agreement
+from .serializers import NoticeSerializer, FaqSerializer, AgreementSerializer
 
 
 class NoticeList(generics.ListAPIView):
@@ -23,3 +24,13 @@ class FaqList(generics.ListAPIView):
 class FaqDetail(generics.RetrieveAPIView):
     queryset = Faq.objects.all()
     serializer_class = FaqSerializer
+
+
+class AgreementList(generics.ListAPIView):
+    queryset = Agreement.objects.all()
+    serializer_class = AgreementSerializer
+
+
+class AgreementDetail(generics.RetrieveAPIView):
+    queryset = Agreement.objects.all()
+    serializer_class = AgreementSerializer
