fde3c5776e3e7a58de7907957bea4afff92c67d7
zealotzealot <hsh0908y@gmail.com>
2019-05-09 18:44:33 +0900

    Add model Agreement to admin page

--- pida/managements/admin.py
+++ pida/managements/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Notice, Faq
+from .models import Notice, Faq, Agreement
 
 
 admin.site.register(Notice)
 admin.site.register(Faq)
+admin.site.register(Agreement)
